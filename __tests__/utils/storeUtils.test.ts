804692990cc20c896bec4be424a06cea5b6e74d7
Jenish <jpat534@gmail.com>
2024-07-12 13:52:09 +1000

    removed test for deleteStoreIfNoItems.
    Will look into that test later.

--- __tests__/utils/storeUtils.test.ts
+++ __tests__/utils/storeUtils.test.ts
@@ -86,7 +86,7 @@ describe("storeUtils", ()=> {
 
             expect(setStoreList).toHaveBeenCalledWith(['Store A']);
             expect(setShoppingList).toHaveBeenCalledWith(expect.objectContaining({}));
-        })
-     })
+        });
+     });
 
 });
