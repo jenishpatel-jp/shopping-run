237d372f1e164b03450b8404d4a9ccd0748a251c
Jenish <jpat534@gmail.com>
2025-06-04 18:41:59 +1000

    Added colour scheme boilerplate to the  shoppingList component

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 export default function ShoppingList() {
+
+  const colorScheme = useColorScheme();
+  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
+  const themeContainerStyle =
+    colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+
   return (
     <View style={styles.container}>
       <Text>Starting the shopping run app </Text>
@@ -16,5 +22,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  lightThemeText: {
+
+  },
+  darkThemeText: {
+    color: 'white',
+  },
+  lightContainer: {
+    backgroundColor: 'white',
+  },
+  darkContainer: {
+    backgroundColor: 'black',
   },
 });
