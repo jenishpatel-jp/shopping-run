c671a6b51ef65c40578156fe2da4c66c68f4b1b7
Jenish <jpat534@gmail.com>
2025-06-23 18:25:09 +1000

    Added a ThemedButton component

--- components/themedComponents/ThemedButton.tsx
+++ components/themedComponents/ThemedButton.tsx
@@ -1,13 +1,28 @@
 import { useThemeColor } from "../../hooks/useThemeColour";
-import { Button, type ButtonProps } from "react-native";
+import { Pressable, type PressableProps } from "react-native";
 
-export type ThemedButtonProps = ButtonProps & {
+export type ThemedButtonProps = PressableProps & {
   lightColor?: string;
   darkColor?: string;
 };
 
 export const ThemedButton = ({
+  style,
   lightColor,
   darkColor,
   ...otherProps
-}: ThemedButtonProps) => {};
+}: ThemedButtonProps) => {
+
+  const backgroundColor = useThemeColor(
+    { light: lightColor, dark: darkColor },
+    "background"
+  );
+
+  const borderColor = useThemeColor(
+    { light: lightColor, dark: darkColor }, 
+    "borderColor" 
+  );
+
+  return <Pressable style={[{ backgroundColor, borderColor }]} {...otherProps} ></Pressable>
+
+};
