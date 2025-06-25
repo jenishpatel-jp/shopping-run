c242845f8e07ccd3ca72c9f05fe6c0cc08003ac8
Jenish <jpat534@gmail.com>
2025-06-25 18:55:42 +1000

    Added header buttons

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,9 @@ import { ThemedText } from "../components/themedComponents/ThemedText";
 import { ThemedTextInput } from "../components/themedComponents/ThemedTextInput";
 import AddStore from "../components/shoppingListComponents/AddStore";
 
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
 import { Stack } from "expo-router";
 
 export default function ShoppingList() {
@@ -17,11 +20,11 @@ export default function ShoppingList() {
             headerTitle: "Shopping List",
             headerRight: () => 
             <View style={styles.headerRightView}>
-              <Pressable>
-
+              <Pressable style={styles.headerRightButton}> 
+                <MaterialCommunityIcons name="store-plus" size={30} color="black" />
               </Pressable>
-              <Pressable>
-                
+              <Pressable style={styles.headerRightButton}> 
+                <Ionicons name="add-circle-outline" size={30} color="black" />
               </Pressable>
 
             </View>,
@@ -47,6 +50,12 @@ const styles = StyleSheet.create({
   },
   headerRightView: {
     flexDirection: "row",
+    justifyContent: "space-around",
     alignItems: "center",
+    width: 100,
+    height: 50,
+  },
+  headerRightButton: {
+    padding: 5,
   }
 });
