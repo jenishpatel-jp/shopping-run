c5cadb14be354d42dc4ce028f574081363c0503b
Jenish <jpat534@gmail.com>
2025-06-29 13:22:31 +1000

    Added styling to the drop down list

--- app/add/addItem.tsx
+++ app/add/addItem.tsx
@@ -42,6 +42,27 @@ const addItem = () => {
         setSelected={(val:string) => setSelected(val)}
         data={data}
         save="value"
+        boxStyles={{
+          borderColor: 'white',
+          borderWidth: 2,
+          padding: 10,
+          width: '80%',
+          marginBottom: 10,
+        }}
+        inputStyles={{
+          color: 'white',
+          fontSize: 24,
+          textAlign: 'center',
+        }}
+        dropdownStyles={{
+          backgroundColor: 'black',
+          borderColor: 'white',
+          borderWidth: 2,
+        }}
+        dropdownTextStyles={{
+          color: 'white',
+          fontSize: 24,
+        }}
       />
       
       <Pressable 
