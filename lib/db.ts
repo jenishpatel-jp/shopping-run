f687bed74ad93a6d57e5c30c9f75c1b1d6e82a14
Jenish <jpat534@gmail.com>
2025-07-01 18:14:45 +1000

    updated the database file to ensure foreign keys can be used

--- lib/db.ts
+++ lib/db.ts
@@ -6,25 +6,30 @@ export const setupDatabase = async (db: SQLiteDatabase) => {
 
     try {
         
+        // Enable foreign key constraints
         await db.execAsync(`PRAGMA foreign_keys = ON;`)
 
+        // Create stores table
         await db.execAsync(`
             CREATE TABLE IF NOT EXISTS stores (
                 storeId INTEGER PRIMARY KEY AUTOINCREMENT,
-                storeName TEXT NOT NULL UNIQUE
+                storeName TEXT NOT NULL
             );
+            `);
 
+        // Create items table
+        await db.execAsync(`
             CREATE TABLE IF NOT EXISTS items (
                 itemId INTEGER PRIMARY KEY AUTOINCREMENT,
                 storeId INTEGER NOT NULL,
-                itemName TEXT NOT NULL UNIQUE,
+                itemName TEXT NOT NULL,
                 completed INTEGER NOT NULL DEFAULT 0,
                 quantity INTEGER NOT NULL,
                 FOREIGN KEY (storeId) REFERENCES stores(storeId) ON DELETE CASCADE
             );
-
             `);
-    }catch(error){
+
+    } catch(error){
         console.error("Error setting up database:", error);
         throw error;
     }
