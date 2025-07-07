d57732f7137748e597bba9f82bbb8d7af49a32e0
Jenish <jpat534@gmail.com>
2025-07-07 19:00:28 +1000

    added a useEffect to update the store state on initial mount

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,37 @@ import { StatusBar } from "expo-status-bar";
 import { Pressable, StyleSheet, Text, useColorScheme, View } from "react-native";
 import { ThemedView } from "../components/themedComponents/ThemedView";
 
-
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
 import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+
+import { useSQLiteContext } from "expo-sqlite";
+import { useDatabase } from "../lib/store";
+
+import { state$ } from "../lib/state"; // Import the global state
 
 export default function ShoppingList() {
 
+  const db = useSQLiteContext();
+  const { fetchStores } = useDatabase(db);
+
   const router = useRouter();
+
+  useEffect(() => {
+    const syncInitialData = async () => {
+      try {
+        const stores = await fetchStores();
+        state$.stores.set(stores); // Update the global state with the fetched stores
+      } catch(error){
+        console.error("Error during initial data sync:", error);
+      }
+    };
+
+    syncInitialData();
+
+  }, [] )
 
   return (
     <>
