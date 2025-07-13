9c0c6a05fa37d82b28ad9d5c212cd141f1c4a77b
Jenish <jpat534@gmail.com>
2025-07-13 17:16:44 +1000

    Added a function to deleteItems from the item Database

--- lib/items.ts
+++ lib/items.ts
@@ -6,12 +6,28 @@ export const useItemDatabase = (db: SQLiteDatabase) => {
     const addItem = async (storeId: number, itemName: string, quantity: number, completed: boolean) => {
         try {
             const result = await db.runAsync(
-                `INSERT INTO items  (storeId, itemName, quanntity, completed) VALUES (?, ?, ?, ?);`,
+                `INSERT INTO items (storeId, itemName, quanntity, completed) VALUES (?, ?, ?, ?);`,
                 [storeId, itemName, quantity, completed ? 1 : 0]
             );
             console.log("Item added successfully:", result);
         } catch (error){
             console.error("Error addting item:", error);
+            throw error;
+        }
+    };
+
+    const deleteItem = async (itemId: number) => {
+        try {
+            const result = await db.runAsync(
+                `DELETE FROM items WHERE itemId = ?;`, [itemId]
+            );
+            if (result.changes > 0){
+                console.log("Item deleted successfully:", itemId);
+            } else {
+                console.log("No item found with ID:", itemId);
+            }
+        } catch (error ){
+            console.error("Error deleting item:", error);
             throw error;
         }
     };
