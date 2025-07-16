3e51a00022fb0381fb8a11f2b1b01ee76a0433a5
Jenish <jpat534@gmail.com>
2025-07-16 18:26:18 +1000

    Added PanResponder to add swipe animation

--- components/shoppingListComponents/Stores.tsx
+++ components/shoppingListComponents/Stores.tsx
@@ -1,27 +1,59 @@
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  PanResponder,
+  Touchable,
+  TouchableOpacity,
+} from "react-native";
 
 import ReanimatedSwipeable from "react-native-gesture-handler/ReanimatedSwipeable";
 import Animated from "react-native-reanimated";
 
 type StoreProps = {
-    storeName: string;
+  storeName: string;
 };
 
-export const Stores = ( { storeName } : StoreProps ) => (
-  
-        <View style={styles.item}>
-            <Text style={styles.title}>{storeName}</Text>
-        </View>
-    
-);
+export const Stores = ({ storeName }: StoreProps) => {
+  const pandResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: (event, gestureState) => {},
+    onPanResponderRelease: (event, gestureState) => {
+      if (gestureState.dx < -50) {
+        console.log("Swipe left detected");
+      }
+    },
+  });
 
+  return (
+    <View style={styles.itemContainer}>
+      <View style={styles.itemContainer} {...pandResponder.panHandlers}>
+        <Text style={styles.title}>{storeName}</Text>
+        <TouchableOpacity
+          onPress={() => console.log("delete")}
+          style={styles.deleteButton}
+        >
+          <Text style={{ color: "white" }}>Delete</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    item: {
+  itemContainer: {
+    marginBottom: 10,
+    color: "white",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+  },
+  item: {
     backgroundColor: "#1E1E1E",
     padding: 20,
     marginVertical: 8,
-    width: "100%",
+    width: "70%",
     borderWidth: 1,
     borderColor: "white",
     alignItems: "center",
@@ -31,5 +63,14 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: "bold",
     color: "white",
-  }
+  },
+
+  deleteButton: {
+    color: "white",
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "30%",
+    right: -150, // moves the delete button off the screen to the right
+  },
 });
