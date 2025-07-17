de6d36d96b91414b54ca0e7813cc3544e1831954
Jenish <jpat534@gmail.com>
2025-07-17 21:56:53 +1000

    Added  Reanimated Swipable components for each store

--- components/shoppingListComponents/Stores.tsx
+++ components/shoppingListComponents/Stores.tsx
@@ -1,29 +1,49 @@
 import {
   Text,
-  View,
   StyleSheet,
-  TouchableOpacity,
 } from "react-native";
 
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import Animated, { 
-  useSharedValue,
+import ReanimatedSwipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import Reanimated, {
+  SharedValue,
   useAnimatedStyle,
-  withTiming,
- } from "react-native-reanimated";
+} from 'react-native-reanimated';
 
 type StoreProps = {
   storeName: string;
 };
 
+function RightAction(prog: SharedValue<number>, drag: SharedValue<number>) {
+  const styleAnimation = useAnimatedStyle(() => {
+    // console.log('showRightProgress:', prog.value);
+    // console.log('appliedTranslation:', drag.value);
+
+    return {
+      transform: [{ translateX: drag.value + 50 }],
+    };
+  });
+
+  return (
+    <Reanimated.View style={styleAnimation}>
+      <Text style={styles.rightAction}>Delete</Text>
+    </Reanimated.View>
+  );
+}
+
+
 export const Stores = ({ storeName }: StoreProps) => {
 
 
-
   return (
-      <View style={styles.itemContainer}>
+      <ReanimatedSwipeable 
+        containerStyle={styles.swipeable}
+        friction={2}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={100}
+        renderRightActions={RightAction}
+      >
         <Text style={styles.title}>{storeName}</Text>
-      </View>
+      </ReanimatedSwipeable>
   );
 };
 
@@ -43,6 +63,28 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: "bold",
     color: "white",
+  },
+  rightAction: { 
+    width: 50, 
+    height: 50, 
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontSize: 14,
+    textAlign: 'center',
+  },
+  separator: {
+    width: '100%',
+    borderTopWidth: 1,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
+  swipeable: {
+    height: 50,
+    backgroundColor: 'black',
+    alignItems: 'center',
   },
 
 });
