c60bc8f62b34595f5bda70fdf9f77b5d3e6ca9e6
Jenish <jpat534@gmail.com>
2025-07-28 17:49:35 +1000

    Added View, Text and styles to the SectionListItem component

--- components/shoppingListComponents/SectionListItem.tsx
+++ components/shoppingListComponents/SectionListItem.tsx
@@ -1,11 +1,33 @@
-import { Text, View } from "react-native";
+import { SQLiteDatabase } from "expo-sqlite";
+import { Text, View, StyleSheet } from "react-native";
 
-const SectionListItem = () => {
+type SectionListItemProps = {
+  item: string;
+  db: SQLiteDatabase;
+};
+
+const SectionListItem = ( { item, db } : SectionListItemProps ) => {
   return (
     <View>
-        <Text>Section List Item</Text>
+        <Text>{item}</Text>
     </View>
   )
 }
 
 export default SectionListItem
+
+const styles = StyleSheet.create({
+  item: {
+    fontSize: 18,
+    padding: 10,
+    backgroundColor: "#f9c2ff",
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 20,
+  },
+
+});
