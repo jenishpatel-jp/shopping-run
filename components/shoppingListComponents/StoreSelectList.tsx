d0c29a71d8e5cf3d7eacb243e9c7f2b554a2de04
Jenish <jpat534@gmail.com>
2025-08-14 21:47:05 +1000

    Updated the colour scheme of the SelectList component to usecolour theme

--- components/shoppingListComponents/StoreSelectList.tsx
+++ components/shoppingListComponents/StoreSelectList.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { SelectList } from "react-native-dropdown-select-list"
+import { useColorScheme } from "react-native";
+import { SelectList } from "react-native-dropdown-select-list";
+import { StyleSheet } from "react-native";
 
 // This component is used to select a store from a dropdown list
 // Doc on the SelectList: https://www.npmjs.com/package/react-native-dropdown-select-list
@@ -14,35 +16,69 @@ type StoreSlectListProp = {
 
 const StoreSelectList  = ( { data, setSelectedStore } : StoreSlectListProp ) => {
 
+  const colorScheme = useColorScheme();
+
+  const themeBackground = colorScheme === "dark" ? styles.darkBackgroundColor : styles.lightBackgroundcolour;
+  const themeBorder = colorScheme === "dark" ? styles.darkBorderColour : styles.lightBorderColour;
+  const themeText = colorScheme === "dark" ? styles.darkColour : styles.lightColour;
+  const themePlaceholder = colorScheme === "dark" ? "#FFE4A1" : "#0A1931";
 
   return (
     <SelectList 
         setSelected={setSelectedStore}
         save="key"
         data={data}
-        boxStyles={{
-          borderColor: '#FFE4A1',
-          borderWidth: 2,
-          padding: 10,
-          width: '80%',
-          marginBottom: 10,
-        }}
+        boxStyles={{...styles.boxStyles, ...themeBorder,}}
         inputStyles={{
-          color: '#FFE4A1',
-          fontSize: 24,
-          textAlign: 'center',
+          ...styles.inputStyles,
+          ...themeText,
         }}
         dropdownStyles={{
-          backgroundColor: '#0A1931',
-          borderColor: '#FFE4A1',
+          ...themeBackground,
+          ...themeBorder,
           borderWidth: 2,
         }}
         dropdownTextStyles={{
-          color: '#FFE4A1',
+          ...themeText,
           fontSize: 24,
         }}
       />
   )
 }
 
-export default StoreSelectList
+export default StoreSelectList;
+
+const styles = StyleSheet.create({
+
+  lightBackgroundcolour: {
+    backgroundColor: "#FFE4A1",
+    },
+  darkBackgroundColor: {
+    backgroundColor: "#0A1931",
+    }, 
+  lightColour: {
+    color: "#0A1931",
+    },
+  darkColour: {
+    color: "#FFE4A1",
+    },
+  lightBorderColour: {
+    borderColor: '#0A1931',
+  },
+  darkBorderColour: {
+    borderColor: '#FFE4A1',
+  },
+  boxStyles: {
+    borderWidth: 2,
+    padding: 10,
+    width: '80%',
+    marginBottom: 10,
+  },
+  inputStyles: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  dropDownStyles: {
+    borderWidth: 2,
+  },
+})
