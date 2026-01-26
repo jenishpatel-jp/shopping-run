d0d421ccd335c0a0c9084c5fcaaf42ca50f4fcd4
Jenish <jpat534@gmail.com>
2026-01-26 17:48:53 +1100

    Added colour scheme to Edit Store page

--- app/page/[editStore].tsx
+++ app/page/[editStore].tsx
@@ -1,14 +1,25 @@
-import { ThemedView } from "../../components/themedComponents/ThemedView";
-import { ThemedText } from "../../components/themedComponents/ThemedText";
+
 import { StyleSheet, Pressable, View, TextInput, Text, useColorScheme } from "react-native";
 import { useSQLiteContext } from "expo-sqlite";
 import { useStoreDatabase } from "../../lib/store";
 
+
 const EditStore = () => {
+    
+    const colourScheme = useColorScheme();
+    const themeBackgroundColour = colourScheme === 'dark' ? styles.darkBackgroundColor : styles.lightBackgroundcolour;
+    const themeBorderColour = colourScheme === 'dark' ? styles.darkBorderColour : styles.lightBorderColour;
+    const themeColour = colourScheme === 'dark' ? styles.darkColour : styles.lightColour;
+
+    const db = useSQLiteContext();
+    const { addStore, fetchStores } = useStoreDatabase(db);
+
+
+
     return (
-        <ThemedView style={styles.container}>
-            <ThemedText style={styles.labelTextInput}>Edit Store Page</ThemedText>
-        </ThemedView>
+        <View style={[styles.container, themeBackgroundColour]}>
+            <Text style={[styles.labelTextInput, themeColour]}>Edit Store Page</Text>
+        </View>
     )
 }
 
@@ -27,5 +38,23 @@ const styles = StyleSheet.create({
             fontWeight: 'bold',
             paddingLeft: 10,
             alignSelf: 'flex-start',
+        },
+    lightBackgroundcolour: {
+            backgroundColor: "#FFE4A1",
+        },
+    darkBackgroundColor: {
+            backgroundColor: "#0A1931",
+        }, 
+    lightColour: {
+            color: "#0A1931",
+        },
+    darkColour: {
+            color: "#FFE4A1",
+        },
+    lightBorderColour: {
+            borderColor: '#0A1931',
+        },
+    darkBorderColour: {
+            borderColor: '#FFE4A1',
         },
 });
