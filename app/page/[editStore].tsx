bf1101c78eff4aa4d5e7d58239cf90ac23ed1560
Jenish <jpat534@gmail.com>
2026-01-28 19:00:49 +1100

    Added Stack.Screen and a border around the Edit Store

--- app/page/[editStore].tsx
+++ app/page/[editStore].tsx
@@ -2,7 +2,7 @@
 import { StyleSheet, Pressable, View, TextInput, Text, useColorScheme } from "react-native";
 import { useSQLiteContext } from "expo-sqlite";
 import { useStoreDatabase } from "../../lib/store";
-import { useRouter, useLocalSearchParams } from "expo-router";
+import { useRouter, useLocalSearchParams, Stack, router } from "expo-router";
 import { use$ } from "@legendapp/state/react";
 import { state$ } from "../../lib/state";
 
@@ -25,21 +25,46 @@ const EditStore = () => {
 
     return (
         <View style={[styles.container, themeBackgroundColour]}>
-            <Text style={[styles.labelTextInput, themeColour]}>Edit Store</Text>
-            <TextInput
-                style={[styles.textInput, themeBorderColour, themeColour]}
-                placeholder={storeName}
-                placeholderTextColor={colourScheme === 'dark' ? "#FFE4A1" : "#0A1931"}
-                selectionColor={colourScheme === 'dark' ? "#FFE4A1" : "#0A1931"}
-            />
-            <Pressable
-                style={[styles.addButton, themeBackgroundColour]}
-                onPress={() => {
-                    // Handle update store action here
-                }}
-            >
-                <Text style={[styles.labelTextInput, themeColour, themeBorderColour]}>Update</Text>
-            </Pressable>
+
+            <Stack.Screen
+                    options={{
+                        headerTitle: 'Add Store',
+                        presentation: 'formSheet',
+                        sheetGrabberVisible: true,
+                        headerLargeTitle: false,
+                        headerShown: true,
+                        headerTitleAlign: 'center',
+                        headerTitleStyle: [styles.headerTitleIcons, themeColour],
+                        headerStyle: { backgroundColor: colourScheme === 'dark' ? '#0A1931' : '#FFE4A1' },
+                        headerLeft: () => 
+                            <Pressable onPressIn={() => router.back()} >
+                                <Text style={[styles.text, themeColour]} >Cancel</Text>
+                            </Pressable>,
+                        headerRight: () =>
+                            <Pressable  >
+                                <Text style={[styles.text, themeColour]} >Save</Text>
+                            </Pressable>
+                    }}
+                
+                />
+
+            <View style={[styles.editStoreContainer, themeBorderColour]}>
+                <Text style={[styles.labelTextInput, themeColour]}>Edit Store</Text>
+                <TextInput
+                    style={[styles.textInput, themeBorderColour, themeColour]}
+                    placeholder={storeName}
+                    placeholderTextColor={colourScheme === 'dark' ? "#FFE4A1" : "#0A1931"}
+                    selectionColor={colourScheme === 'dark' ? "#FFE4A1" : "#0A1931"}
+                />
+                <Pressable
+                    style={[styles.addButton, themeBackgroundColour]}
+                    onPress={() => {
+                        // Handle update store action here
+                    }}
+                >
+                    <Text style={[styles.labelTextInput, themeColour, themeBorderColour]}>Update</Text>
+                </Pressable>
+            </View>
         </View>
     )
 }
@@ -53,6 +78,16 @@ const styles = StyleSheet.create({
             width: '100%',
             flex: 1,
         },
+    editStoreContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 8,
+            width: '90%',
+            borderWidth: 2,
+            padding: 10,
+            marginBottom: 100,
+            textAlign: 'left',
+    },
     labelTextInput: {
             fontSize: 20,
             marginBottom: 10,
@@ -87,6 +122,16 @@ const styles = StyleSheet.create({
             paddingLeft: 10,
             
         },
+    text: {
+            fontSize: 18,
+            marginLeft: 10,
+            fontWeight: 'bold',
+        },
+    headerTitleIcons: {
+            fontSize: 20, 
+            fontWeight: 'bold'
+        },
+
     lightBackgroundcolour: {
             backgroundColor: "#FFE4A1",
         },
